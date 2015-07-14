0be7824fc995c2155dcec32d3230c16dd046ea09
seaders <seaders69@gmail.com>
2015-07-14 19:53:02 +0100

    Initial commit.

---
+++ Assets/MainScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using Amazon.CognitoIdentity;
+using Amazon;
+
+public class MainScript : MonoBehaviour
+{
+	void Start()
+	{
+		new CognitoAWSCredentials("", RegionEndpoint.USEast1);
+	}
+}
